b01fee3191814c81fff7784ef830d0670f4fb096
thefirsty <106032265+thefirsty@users.noreply.github.com>
2025-04-02 19:06:39 +0300

    add basic service

---
+++ part4/Grocery/Service/Service/ExtensionService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Repository.Repository;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public static class ExtensionService
+    {
+        public static IServiceCollection AddServiceExtension(this IServiceCollection services)
+        {
+
+            services.AddRepository();
+
+            services.AddScoped<IService<UserDto>, UserService>();
+
+            services.AddScoped<IService<PostDto>, PostService>();
+
+            services.AddScoped<IService<FollowerDto>, FollowerService>();
+
+            services.AddScoped<IService<CommentDto>, CommentService>();
+
+            services.AddScoped<IService<ChatMessageDto>, ChatMessageService>();
+
+            services.AddAutoMapper(typeof(MyMapper));
+
+            return services;
+        }
+    }
+}
