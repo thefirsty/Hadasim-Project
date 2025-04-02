b84870ed753a67cf4760ea377552ea0ac8a6db1a
thefirsty <106032265+thefirsty@users.noreply.github.com>
2025-04-02 20:31:35 +0300

    add database on mock

---
+++ part4/Grocery/Mock/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mock
+{
+    public class DataBase : DbContext, IContext
+    {
+        public DbSet<Achievement> achivevements { get; set; }
+        public DbSet<Challenge> challenges { get; set; }
+        public DbSet<ChallengeParticipant> challengeParticipants { get; set; }
+        public DbSet<ChatMessage> chatMessages { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Follower> followers { get; set; }
+
+        public void Save()
+        {
+            SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;database=sport_network;trusted_connection=true");
+        }
+
+
+    }
+}
+
